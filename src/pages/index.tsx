f65a91edf17d94486bf4a2674c503b94261c4d33
Milton Chung <miltonjchung@gmail.com>
2023-03-06 22:58:31 -0800

    updated all project links, better transition blur

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Landing } from "@/components/Landing";
 import styles from "@/styles/Home.module.scss";
 import Image from "next/image";
-import DashboardPic from "../../public/assets/projects/dashboard.png";
 import { projects } from "@/utils";
 
 export default function Home() {
@@ -24,7 +23,13 @@ export default function Home() {
             >
               Frontend Mentor
             </a>
-            . I hope you enjoy them!
+            . Each project has three links: Github, Live, and Frontend Mentor
+            solutions page. I documented every project I did on each of the
+            project Github links. I share what the design looks like, challenges
+            I faced, what I learned, and time spent on each project. Note that I
+            started doing these projects back when I started to learn web
+            development so some of the solutions are not the best, but it&apos;s
+            a great way to look back and see how far I&apos;ve come.
           </p>
         </section>
 
@@ -49,13 +54,28 @@ export default function Home() {
                     {project.title}
                   </a>
                   <div className={styles.links}>
-                    <a href={project.githubLink} className="primary-link-btn">
+                    <a
+                      href={project.githubLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="primary-link-btn"
+                    >
                       Github
                     </a>
-                    <a href={project.projectLink} className="primary-link-btn">
+                    <a
+                      href={project.projectLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="primary-link-btn"
+                    >
                       Live
                     </a>
-                    <a href={project.feMentorLink} className="primary-link-btn">
+                    <a
+                      href={project.feMentorLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="primary-link-btn"
+                    >
                       FE Mentor
                     </a>
                   </div>
--- src/utils.ts
+++ src/utils.ts
@@ -21,206 +21,246 @@ import ArticlePreviewComponent from "../public/assets/projects/article_preview.p
 import SignUpComponent from "../public/assets/projects/sign_up.png";
 import HuddleLandingPage from "../public/assets/projects/Huddle_landing.png";
 
-// TODO: update project links, github links, feMentor links, and date completed
 // TODO: tinypng images
 // TODO: delay loading of images until they are in view
 export const projects = [
   {
     title: "Social Media Dashboard",
     imageSrc: Dashboard,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/SocialMediaDashboard/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/SocialMediaDashboard",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/social-media-dashboard-using-scss-za8W5UzBp",
+    dateCompleted: "February 2021",
   },
   {
     title: "URL Shortener Api",
     imageSrc: UrlShortener,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink: "https://url-shortener-api.netlify.app/",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/UrlShortener",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/url-shortener-api-with-loading-animation-in-react-otxHfIL1d",
+    dateCompleted: "March 2021",
   },
   {
     title: "Countries Api",
     imageSrc: CountriesApi,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink: "https://countries-rest-api-solution.netlify.app/",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/CountriesRestApi",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/countries-rest-api-B5niGgoeK",
+    dateCompleted: "April 2021",
   },
   {
     title: "IP Address Tracker",
     imageSrc: IpTracker,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/IPAddressTracker/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/IPAddressTracker",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/ip-address-tracker-with-loading-animation-jGfmGf27p",
+    dateCompleted: "January 2021",
   },
   {
     title: "Profile Card Component",
     imageSrc: ProfileCard,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/ProfileCardComponent/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/ProfileCardComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/profile-card-component-sgTEXDi_f",
+    dateCompleted: "January 2021",
   },
   {
     title: "Huddle Full Page Curved",
     imageSrc: HuddleFullPageCurved,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/HuddleFullPageCurved/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/HuddleFullPageCurved",
     feMentorLink:
       "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+    dateCompleted: "December 2020",
   },
   {
     title: "Fylo Full Page Dark",
     imageSrc: FyloFullPageDark,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/FyloLandingDarkTheme/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/FyloLandingDarkTheme",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/fylo-dark-theme-landing-page-D8BLjHoIj",
+    dateCompleted: "November 2020",
   },
   {
     title: "Huddle Full Page",
     imageSrc: HuddleFullPage,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/HuddleFullPage/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/HuddleFullPage",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/huddle-landing-page-with-alternating-feature-blocks-ltC6OlY-N",
+    dateCompleted: "November 2020",
   },
   {
     title: "Project Tracking Component",
     imageSrc: ProjectTrackingComponent,
     projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/ProjectTrackingComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/project-tracking-intro-component-3d4Vrms_S",
+    dateCompleted: "November 2020",
   },
   {
     title: "Fylo Landing Page",
     imageSrc: FyloLandingPage,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/FyloLandingPage/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/FyloLandingPage",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/fylo-landing-page-with-two-column-layout-mq1SuQOd5",
+    dateCompleted: "November 2020",
   },
   {
     title: "Testimonial Grid Section",
     imageSrc: TestimonialGridSection,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/TestimonialGridSectionChallenge/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/TestimonialGridSectionChallenge",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/testimonials-grid-section-VZpDccn65",
+    dateCompleted: "October 2020",
   },
   {
     title: "Ping Coming Soon Page",
     imageSrc: PingComingSoonPage,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/PingComingSoonPage/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/PingComingSoonPage",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/ping-coming-soon-page-hSfhegb-5",
+    dateCompleted: "October 2020",
   },
   {
     title: "Huddle Landing Page",
     imageSrc: HuddleLandingPage,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/HuddleLandingPage/index.html/",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/HuddleLandingPage",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/huddle-landing-page-VFxpyjObK",
+    dateCompleted: "October 2020",
   },
   {
     title: "Sign Up Form Component",
     imageSrc: SignUpComponent,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/SignUpFormComponent/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/SignUpFormComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/intro-component-sign-up-form-XBuI0TDrn",
+    dateCompleted: "October 2020",
   },
   {
     title: "Single Price Grid Component",
     imageSrc: SinglePriceGridComponent,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/SinglePriceGridComponent/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/SinglePriceGridComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/price-grid-component-sJl-qMMXg",
+    dateCompleted: "October 2020",
   },
   {
     title: "Base Apparel Coming Soon",
     imageSrc: BaseApparel,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/BaseApparelComingSoon/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/BaseApparelComingSoon",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/base-apparel-coming-soon-page-mJ_iIXSVm",
+    dateCompleted: "October 2020",
   },
   {
     title: "FAQ Accordion Card",
     imageSrc: FAQAccordionCard,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/FaqAccordionCard/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/FaqAccordionCard",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/faq-accordion-card-KBip959hY",
+    dateCompleted: "Oct 2020",
   },
   {
     title: "Four Card Feature Section",
     imageSrc: FourCardFeatureSection,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/FourCardFeatureSection/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/FourCardFeatureSection",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/four-card-feature-section-aTnFqBH9C",
+    dateCompleted: "September 2020",
   },
   {
     title: "Coding Bootcamp Testimonial",
     imageSrc: BootcampTestimonialsSlider,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/CodingBootcampTestimonialsSlider/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/CodingBootcampTestimonialsSlider",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/coding-bootcamp-testimonials-slider-XU5_ULuNB",
+    dateCompleted: "September 2020",
   },
   {
     title: "Fylo Data Storage Component",
     imageSrc: FyloStorageComponent,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/FyloDataStorageComponent/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/FyloDataStorageComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/fylo-data-storage-component-ZAgZnAbfX",
+    dateCompleted: "September 2020",
   },
   {
     title: "Social Proof Section",
     imageSrc: SocialProofSection,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/SocialProofSection/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/SocialProofSection",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/social-proof-section-wcA77kGml",
+    dateCompleted: "September 2020",
   },
   {
     title: "Article Preview Component",
     imageSrc: ArticlePreviewComponent,
-    projectLink: "https://social-media-dashboard-ten.vercel.app/",
-    githubLink: "asd",
+    projectLink:
+      "https://miltonchung.github.io/FrontendMenterChallenge/ArticlePreviewComponent/index.html",
+    githubLink:
+      "https://github.com/MiltonChung/FrontendMenterChallenge/tree/master/ArticlePreviewComponent",
     feMentorLink:
-      "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
-    dateCompleted: "Jan 2021",
+      "https://www.frontendmentor.io/solutions/article-preview-component-IA07Vl6Rj",
+    dateCompleted: "September 2020",
   },
 ];
